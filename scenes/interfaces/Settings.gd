4c67040d8839ef557ea3c6c90c0fe4c0d911ca0b
magwoo <magwood51@yandex.ru>
2023-04-11 21:07:00 +0300

    update settings animations

--- scenes/interfaces/Settings.gd
+++ scenes/interfaces/Settings.gd
@@ -1,24 +1,32 @@
 extends CanvasLayer
 
 
+const FADE_SPEED: float = 4.0
+
 onready var control: Control = $Control
 
 var is_destroyed: bool = false
 var selected_button: Button
 
+var tween: Tween = Tween.new()
+
 
 func _ready() -> void:
+	self.add_child(tween)
 	control.modulate.a = 0
-	self.offset.y = -120
+
+	tween.interpolate_property(
+		self, 'offset', Vector2(0.0, -120.0), Vector2(0.0, 0.0),
+		0.3, Tween.TRANS_BACK, Tween.EASE_OUT
+	)
+	tween.start()
+
 
 
 func _process(_delta: float) -> void:
-	self.offset.y = lerp(self.offset.y, 0, Global.lerp_index)
-
 	if is_destroyed:
-		control.modulate.a -= 4 * _delta
-		if control.modulate.a < 0:
-			self.queue_free()
+		control.modulate.a -= FADE_SPEED * _delta
+		if control.modulate.a < 0.0: self.queue_free()
 	else:
-		control.modulate.a += 4 * _delta
-		control.modulate.a = min(1, control.modulate.a)
+		control.modulate.a += FADE_SPEED * _delta
+		control.modulate.a = min(1.0, control.modulate.a)
